01522b2af5f65a2a35f37ae16ebdd5ffbec3a039
Ram <ram.marshivane@exaqode.com>
2026-04-03 16:44:10 +0530

    Links Handling

---
+++ src/test/java/com/demoqa/Links_Handling.java
@@ -0,0 +1,18 @@
+package com.demoqa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Links_Handling {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+		System.out.println(driver.findElements(By.tagName("a")).size());
+	}
+}
