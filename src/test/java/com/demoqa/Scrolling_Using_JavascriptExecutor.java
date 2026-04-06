727499a2c0bb73774e87d19c82c034d38a377bc7
Ram <ram.marshivane@exaqode.com>
2026-04-06 19:32:48 +0530

    updated JS class

---
+++ src/test/java/com/demoqa/Scrolling_Using_JavascriptExecutor.java
@@ -0,0 +1,22 @@
+package com.demoqa;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Scrolling_Using_JavascriptExecutor {
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,500)");
+		Thread.sleep(5000);
+		js.executeAsyncScript("document.querySelector('.tableFixHead').scrollTop=5000");
+		Thread.sleep(5000);
+		driver.quit();
+	}
+}
