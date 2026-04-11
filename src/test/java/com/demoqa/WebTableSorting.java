80bef8a241e882193db3fd9c1b950156ec123cd5
Ram <ram.marshivane@exaqode.com>
2026-04-11 11:52:57 +0530

    Added pagination scenario

--- src/test/java/com/demoqa/WebTableSorting.java
+++ src/test/java/com/demoqa/WebTableSorting.java
@@ -16,23 +16,63 @@ public class WebTableSorting extends BaseTest {
 		setUp();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
 
-		System.out.println("Web page title is: "+driver.findElement(By.cssSelector(".brand.greenLogo")).getText());
-		
+		System.out.println("Web page title is: " + driver.findElement(By.cssSelector(".brand.greenLogo")).getText());
 
-		driver.findElement(By.xpath("//tr//th[1]")).click();
+		// click on column
 
-		// Elements List
-		List<WebElement> elementsList = driver.findElements(By.xpath("//tr//td[1]"));
+		driver.findElement(By.xpath("//tr/th[1]")).click();
 
-		// Capture list of all original elements
+		// capture all webelements into list
+
+		List<WebElement> elementsList = driver.findElements(By.xpath("//tr/td[1]"));
+
+		// capture text of all webelements into new(original) list
+
 		List<String> originalList = elementsList.stream().map(s -> s.getText()).collect(Collectors.toList());
 
-		// Sort original List
+		// sort on the original list of step 3 -> sorted list
+
 		List<String> sortedList = originalList.stream().sorted().collect(Collectors.toList());
 
-		// Compare original List vs Sorted list
+		// compare original list vs sorted list
 
 		Assert.assertTrue(originalList.equals(sortedList));
+
+		List<String> price;
+
+		// scan the name column with getText ->Beans->print the price of the Rice
+
+		do
+
+		{
+
+			List<WebElement> rows = driver.findElements(By.xpath("//tr/td[1]"));
+
+			price = rows.stream().filter(s -> s.getText().contains("Rice"))
+
+					.map(s -> getPriceVeggie(s)).collect(Collectors.toList());
+
+			price.forEach(a -> System.out.println(a));
+
+			if (price.size() < 1)
+
+			{
+
+				driver.findElement(By.cssSelector("[aria-label='Next']")).click();
+
+			}
+
+		} while (price.size() < 1);
+
+	}
+
+	private static String getPriceVeggie(WebElement s) {
+
+		// TODO Auto-generated method stub
+
+		String pricevalue = s.findElement(By.xpath("following-sibling::td[1]")).getText();
+
+		return pricevalue;
 
 	}
 
