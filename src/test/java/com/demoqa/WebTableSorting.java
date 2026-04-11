1e432157a0548fc7abebb10aea21a92a1ec3cc9a
Ram <ram.marshivane@exaqode.com>
2026-04-11 11:23:51 +0530

    WebTable sorting using Java Stream

--- src/test/java/com/demoqa/WebTableSorting.java
+++ src/test/java/com/demoqa/WebTableSorting.java
@@ -1,9 +1,13 @@
 package com.demoqa;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class WebTableSorting extends BaseTest{
 
@@ -17,8 +21,23 @@ public class WebTableSorting extends BaseTest{
 		  String ParentWindow=it.next();
 		  String ChildWindow=it.next();
 		  System.out.println(driver.findElement(By.cssSelector(".brand.greenLogo")).getText());
-		  Thread.sleep(4000);
-		  driver.switchTo().window(ParentWindow);
+//		  Thread.sleep(4000);
+//		  driver.switchTo().window(ParentWindow);
+		 
+		  driver.findElement(By.xpath("//tr//th[1]")).click();
+		  
+		  //Elements List
+		 List<WebElement> elementsList =driver.findElements(By.xpath("//tr//td[1]"));
+		 
+		 //Capture list of all original elements
+		 List<String> originalList=elementsList.stream().map(s->s.getText()).collect(Collectors.toList());
+		 
+		 //Sort original List
+		 List <String> sortedList=originalList.stream().sorted().collect(Collectors.toList());
+		 
+		 //Compare original List vs Sorted list
+		 
+		 Assert.assertTrue(originalList.equals(sortedList));
 		 
 	}
 
