ae5cb3cc2562c8724042e283d52ea0f89f6f76e5
Ram <ram.marshivane@exaqode.com>
2026-04-11 11:49:46 +0530

    Sorted Web Table

--- src/test/java/com/demoqa/WebTableSorting.java
+++ src/test/java/com/demoqa/WebTableSorting.java
@@ -9,36 +9,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
-public class WebTableSorting extends BaseTest{
+public class WebTableSorting extends BaseTest {
 
 	public static void main(String[] args) throws InterruptedException {
 
 		setUp();
-		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
-		driver.findElement(By.xpath("//a[contains(text(),'Top Deals')]")).click();
-		 Set<String> tabs=driver.getWindowHandles();
-		  Iterator<String> it=tabs.iterator();
-		  String ParentWindow=it.next();
-		  String ChildWindow=it.next();
-		  System.out.println(driver.findElement(By.cssSelector(".brand.greenLogo")).getText());
-//		  Thread.sleep(4000);
-//		  driver.switchTo().window(ParentWindow);
-		 
-		  driver.findElement(By.xpath("//tr//th[1]")).click();
-		  
-		  //Elements List
-		 List<WebElement> elementsList =driver.findElements(By.xpath("//tr//td[1]"));
-		 
-		 //Capture list of all original elements
-		 List<String> originalList=elementsList.stream().map(s->s.getText()).collect(Collectors.toList());
-		 
-		 //Sort original List
-		 List <String> sortedList=originalList.stream().sorted().collect(Collectors.toList());
-		 
-		 //Compare original List vs Sorted list
-		 
-		 Assert.assertTrue(originalList.equals(sortedList));
-		 
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+
+		System.out.println("Web page title is: "+driver.findElement(By.cssSelector(".brand.greenLogo")).getText());
+		
+
+		driver.findElement(By.xpath("//tr//th[1]")).click();
+
+		// Elements List
+		List<WebElement> elementsList = driver.findElements(By.xpath("//tr//td[1]"));
+
+		// Capture list of all original elements
+		List<String> originalList = elementsList.stream().map(s -> s.getText()).collect(Collectors.toList());
+
+		// Sort original List
+		List<String> sortedList = originalList.stream().sorted().collect(Collectors.toList());
+
+		// Compare original List vs Sorted list
+
+		Assert.assertTrue(originalList.equals(sortedList));
+
 	}
 
 }
