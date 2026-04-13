e07cc69beeaeb5384bdcf03764ca0f79cc492775
Ram56100M <rammarshivane@gmail.com>
2026-04-13 21:14:53 +0530

    Updated Author

---
+++ src/test/java/testng/Day5.java
@@ -0,0 +1,28 @@
+package testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Day5 {
+
+	@Test
+	public void MobileAndroidLoginCarLoan() {
+
+		System.out.println("Android Mobile login Car");
+	}
+
+	
+	
+	@Test
+	public void MobileiOSLoginCarLoan() {
+
+		System.out.println("iOS Mobile login Car");
+	}
+	
+	@Test
+	public void MobileloginAPICarLoan() {
+
+		System.out.println("MobileloginAPICarLoan");
+	}
+
+}
