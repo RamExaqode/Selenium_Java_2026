cd61e26537a5b38b653c0c2366935146b5bde969
Ram56100M <rammarshivane@gmail.com>
2026-04-14 13:54:27 +0530

    Healenium Selenium

--- src/test/java/testng/TestNG_Annotations.java
+++ src/test/java/testng/TestNG_Annotations.java
@@ -1,5 +1,25 @@
 package testng;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class TestNG_Annotations {
+
+	@AfterTest
+	public void afterTestMethod() {
+		System.out.println("I will execute last");
+	}
+
+	@Test
+	public void testMethod() {
+
+		System.out.println("Test method");
+	}
+
+	@BeforeTest
+	public void beforeTestMethod() {
+		System.out.println("I will execute before all the tests");
+	}
 
 }
